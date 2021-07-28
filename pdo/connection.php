64213130be926e875cd564a58acffda49d5b8ce0
Jose <jose.tgarrido@gmail.com>
2021-07-27 22:19:02 -0400

    change info server connection

--- pdo/connection.php
+++ pdo/connection.php
@@ -1,9 +1,9 @@
 <?php
 //info server connection
-$server   = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'db_local';
+$server   = 'here_server_name';
+$username = 'here_user';
+$password = 'here_password';
+$database = 'here_data_base';
 
 try {
   // Driver / Server /database
@@ -11,7 +11,7 @@ try {
 
   // Forcing connection to throws an exception type error
   $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-  
+
 } catch (PDOException $error) {
   // Die: Print message and exit
   die('Connection Failed: ' . $error->getMessage());
